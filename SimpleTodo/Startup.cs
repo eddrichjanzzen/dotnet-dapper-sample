0ba3f3c39b513cbda8f66a5b6fd0bfaf8643404a
Eddrich Janzzen Ang <eddrichang@gmail.com>
2020-07-19 13:45:08 +0800

    add swashbuckle docs

--- SimpleTodo/Startup.cs
+++ SimpleTodo/Startup.cs
@@ -37,11 +37,25 @@ namespace SimpleTodo
             //add dependency injection for todo item provider and service
             services.AddTransient<ITodoItemProvider, TodoItemProvider>();
             services.AddTransient<ITodoItemService, TodoItemService>();
+
+            //add swagger
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dapper dotnet Sample: Todo API");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
